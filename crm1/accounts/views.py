aa6e1139e335d950665f63a9b93ace5f5c6eaa00
nightfury1103 <huyquochoan2@gmail.com>
2022-04-17 15:13:31 +0700

    version_1

---
+++ crm1/accounts/views.py
@@ -0,0 +1,36 @@
+from functools import total_ordering
+from multiprocessing import context
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from accounts.models import Customer, Order, Product
+# Create your views here.
+
+def home(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 
+            'customers': customers,
+            'total_customers': total_customers,
+            'total_orders': total_orders,
+            'delivered': delivered,
+            'pending': pending}
+
+    return render(request, 'accounts/dashboard.html', context)
+
+def products(request):
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
+
+def customer(request):
+    return render(request, 'accounts/customer.html')
+
+
