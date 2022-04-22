00529e23f1c811f205c87e9dff8d8d48c67b9781
nightfury1103 <huyquochoan2@gmail.com>
2022-04-22 15:14:33 +0700

    ver_4

--- crm1/accounts/forms.py
+++ crm1/accounts/forms.py
@@ -3,6 +3,15 @@ from django.forms import ModelForm
 from .models import Order
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+
+from .models import *
+
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['user']
 
 class OrderForm(ModelForm):
     class Meta:
